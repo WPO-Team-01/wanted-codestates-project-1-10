0d0827c9a73678c103db5419007aa22eacd1082a
Yongrok <94chl@naver.com>
2022-03-03 14:20:03 +0900

    add: MainPage 마크업

---
+++ src/components/CustomInput/CustomInput.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./CustomInput.module.scss";
+
+const { inputBox, inputContent, inputText, buttonBox } = styles;
+
+const CustomInput = ({ placeholder, onChange, value, disable }) => {
+  return (
+    <div className={classNames(inputBox)}>
+      <div className={classNames(inputContent)}>
+        <div className={classNames(inputText)}>
+          <div>&#128269;</div>
+          <input
+            type="text"
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+            disabled={disable}
+          />
+        </div>
+        <div className={classNames(buttonBox)}>
+          <button>검색</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomInput;
