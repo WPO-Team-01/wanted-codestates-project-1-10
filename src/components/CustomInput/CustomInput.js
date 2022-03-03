647b9cdbeaa34f9bde6122d54c329a84291254d4
sun0-1106 <sy.choi1106@gmail.com>
2022-03-04 02:31:29 +0900

    Add:Make local-caching

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,19 +1,37 @@
-import React from "react";
-import classNames from "classnames";
-import styles from "./CustomInput.module.scss";
+import React from 'react';
+import classNames from 'classnames';
+import styles from './CustomInput.module.scss';
+import { useEffect, useState } from 'react';
+import { api } from '../../axios';
 
 const { inputBox, inputContent, inputText, buttonBox } = styles;
 
-const CustomInput = ({ placeholder, onChange, value, disable }) => {
+const CustomInput = ({ placeholder, value, disable }) => {
+  const handleInputChange = e => {
+    const searchInput = e.target.value;
+    const checkCashe = localStorage.getItem(searchInput);
+    //이미 입력한적 있는 단어라면
+    if (checkCashe) {
+      api.get(searchInput).then(res => {
+        localStorage.getItem(searchInput);
+      });
+    } else {
+      //처음 입력하는 단어라면
+      api.get(searchInput).then(res => {
+        localStorage.setItem(searchInput, JSON.stringify(res.data));
+      });
+    }
+  };
+
   return (
     <div className={classNames(inputBox)}>
       <div className={classNames(inputContent)}>
         <div className={classNames(inputText)}>
           <div>&#128269;</div>
           <input
-            type="text"
+            type='text'
             placeholder={placeholder}
-            onChange={onChange}
+            onChange={e => handleInputChange(e)}
             value={value}
             disabled={disable}
           />
